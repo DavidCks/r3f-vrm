7ee54f03c47df510197174bd3265575ea8a75282
David Christ <davidckss@proton.me>
2024-08-29 19:07:23 +0900

    add some documentation + loop ability

--- src/utils/ExpressionManager.ts
+++ src/utils/ExpressionManager.ts
@@ -13,11 +13,51 @@ import {
   MotionExpression,
 } from "./MotionExpressionManager.ts";
 import { MotionConversionWorkerClient } from "./MotionExpressionWorkerClient.ts";
+import { LoopRepeat, LoopOnce } from "three";
 
 export interface ExpressionInput {
+  /**
+   *  ```ts
+   *  FaceExpression {
+   *    duration: number;
+   *    angry?: number; // between 0 and 1
+   *    happy?: number; // between 0 and 1
+   *    neutral?: number; // between 0 and 1
+   *    relaxed?: number; // between 0 and 1
+   *    sad?: number; // between 0 and 1
+   *    surprised?: number; // between 0 and 1
+   * }
+   * ```
+   */
   faceExpressions?: FaceExpression[];
+  /**
+   *  ```ts
+   *  MouthExpression {
+   *    duration: number;
+   *    aa?: number; // between 0 and 1
+   *    ee?: number; // between 0 and 1
+   *    ih?: number; // between 0 and 1
+   *    oh?: number; // between 0 and 1
+   *    ou?: number; // between 0 and 1
+   *  }
+   * ```
+   */
   mouthExpressions?: MouthExpression[];
+  /**
+   *  ```ts
+   *  MotionExpression {
+   *    clip: THREE.AnimationClip;
+   *    duration?: number; // will default to clip duration if not provided
+   *  }
+   * ```
+   */
   motionExpressions?: MotionExpression[];
+  /**
+   * 2201: THREE.LoopRepeat
+   *
+   * 2200: THREE.LoopOnce
+   */
+  loopMotion?: typeof LoopRepeat | typeof LoopOnce;
 }
 
 export class ExpressionManager {
@@ -52,7 +92,10 @@ export class ExpressionManager {
     }
 
     if (expressionInput.motionExpressions) {
-      this.motion.applyExpressions(expressionInput.motionExpressions);
+      this.motion.applyExpressions(
+        expressionInput.motionExpressions,
+        expressionInput.loopMotion
+      );
     }
   }
 
