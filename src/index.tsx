4c7f9813b75c9d1e62f1bb0c908c120ffdb89baa
David Christ <davidckss@proton.me>
2024-09-14 19:33:28 +0900

    (change) change default imports to explicit ones

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Canvas } from "@react-three/fiber";
 import { VRMAvatar } from "./VRMAvatar";
-import VRMManager from "./utils/VRMManager";
+import { VRMManager } from "./utils/VRMManager";
 import { Grid, OrbitControls } from "@react-three/drei";
 import { Vector3 } from "three";
 import * as THREE from "three";
